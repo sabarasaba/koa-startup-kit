d5801976b6317c41bc6ff66ef64353e955e5822f
Ignacio Rivas <rivasign@gmail.com>
2017-10-07 07:53:21 +0300

    Actually send the email when /contact gets hit

--- src/controllers/static.js
+++ src/controllers/static.js
@@ -1,3 +1,10 @@
+const { sendMail } = require('../helpers/mailer')
+
+const {
+  APP_NAME,
+  CONTACT_EMAIL
+} = process.env
+
 async function index (ctx) {
   await ctx.render('index')
 }
@@ -20,6 +27,14 @@ async function contact (ctx) {
 }
 
 async function contactPost (ctx) {
+  // No need to block rendering for sending an email..
+  sendMail({
+    to: CONTACT_EMAIL,
+    from: ctx.body.email,
+    subject: `${APP_NAME}: New message from ${ctx.body.name}`,
+    content: ctx.body.body
+  }).catch(e => console.log(e)) // TODO: Properly log it with winston..
+
   ctx.flash('success', ['We got your message, we\'ll get back to you soon :)'])
   ctx.redirect('/contact')
 }
