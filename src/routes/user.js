6300d8acf307d03a48c65be334c9c786d32fbfe3
Ignacio Rivas <rivasign@gmail.com>
2017-10-11 13:55:05 +0200

    Add isAuthenticated middleware

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,14 @@
 const Router = require('koa-router')
 const controller = require('../controllers/user')
 const validator = require('../middlewares/schema-validator')
+const isAuthenticated = require('../middlewares/is-authenticated')
 const userModel = require('../models/user')
 
 const router = new Router()
 
-router.get('/settings', controller.settings)
-router.post('/settings/profile', validator(userModel.settingsSchema, '/settings'), controller.profilePost)
-router.post('/settings/password', validator(userModel.resetSchema, '/settings'), controller.passwordPost)
-router.post('/settings/delete', controller.deletePost)
+router.get('/settings', isAuthenticated, controller.settings)
+router.post('/settings/profile', isAuthenticated, validator(userModel.settingsSchema, '/settings'), controller.profilePost)
+router.post('/settings/password', isAuthenticated, validator(userModel.resetSchema, '/settings'), controller.passwordPost)
+router.post('/settings/delete', isAuthenticated, controller.deletePost)
 
 module.exports = router
