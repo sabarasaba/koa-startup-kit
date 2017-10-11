2c3c18d5d2b9003fafb97caa75532398edadff6e
Ignacio Rivas <rivasign@gmail.com>
2017-10-11 15:17:19 +0200

    Add fusebox bundler

--- fuse.js
+++ fuse.js
@@ -1,18 +1,38 @@
-const { FuseBox, WebIndexPlugin, SassPlugin, CSSPlugin } = require('fuse-box')
+const { FuseBox, SassPlugin, CSSPlugin } = require('fuse-box')
+const find = require('find')
+
+const homeDir = 'src/public/app'
+
+const runBundles = (fuse) => {
+  return new Promise((resolve) => {
+    find.file(/index\.js$/, homeDir, (files) => {
+      files.forEach((file) => {
+        const bundlePath = file.replace(homeDir, '')
+
+        fuse.bundle(bundlePath.replace('.js', ''))
+          .cache(false)
+          .watch()
+          .instructions(`> ${bundlePath}`)
+      })
+
+      resolve()
+    })
+  })
+}
 
 const fuse = FuseBox.init({
-  homeDir: 'src/public/app',
+  homeDir,
+  target: 'browser',
   output: 'src/public/dist/$name.js',
   plugins: [
-    WebIndexPlugin({ template: 'src/index.html' }),
-
-    [SassPlugin(), CSSPlugin()]
+    [SassPlugin(), CSSPlugin({
+      outFile: (file) => `src/public/dist/${file}`
+    })]
   ]
 })
 
 fuse.dev()
 
-fuse.bundle('app').hmr().watch()
-  .instructions(`>index.js`)
-
-fuse.run()
+runBundles(fuse).then(() => {
+  fuse.run()
+})
